c362d2c3551627e9ee5fa2c40206c54e4a8e2392
Marzena <marzena.szopinska090@gmail.com>
2019-02-16 21:14:41 +0100

    prepared the neccessary tools to complete the project

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-
   const qwerty = document.getElementById('qwerty');
   const phrase = document.getElementById('phrase');
-
+  const phrases = [
+  'impossible is for the unwilling',
+  'take the risk or lose the chance',
+  'focus on the good',
+  'enjoy today',
+  'never stop dreaming'
+  ];
   const overlay = document.getElementById('overlay');
   // keeps track of the number of guesses the player has missed
   let missed = 0;
@@ -21,8 +26,29 @@ document.addEventListener("DOMContentLoaded", () => {
         e.target.style.cursor = 'pointer';
     }
   });
+  // function that randomly choosees a phrase from the phrases array and split that phrase into a new array of characters.
+  // The function returns the new character array.
+  function getRandomPhraseAsArray(arr) {
 
-  // 
+  }
+
+  // loops through an array of characters
+  function addPhraseToDisplay(arr) {
+
+  }
+
+  //const phraseArray = getRandomPhraseAsArray(phrases);
+  //addPhrasetoDisplay(phraseArray);
+
+  function checkLetter(letter) {
+
+  }
+
+  // each time the player guesses a letter, the function checks whether the game has been won or lost
+  function checkWin() {
+
+  }
+
 
 
 });
